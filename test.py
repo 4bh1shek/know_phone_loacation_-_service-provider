a7d97a21757c481b7ed3f2fc341246032c7a1657
Abhishek Kumar Upadhyay <77145757+4bh1shek@users.noreply.github.com>
2022-03-05 23:01:57 +0530

    Add files via upload
    just enter the phone number in place of +XX XXXXXXXXXX with the country code and just simply save it, further run the test.py file to see results.

---
+++ test.py
@@ -0,0 +1,16 @@
+
+import phonenumbers
+from phone import number
+
+from phonenumbers import geocoder
+ch_number= phonenumbers.parse(number, "CH" )
+print(geocoder.description_for_number ( ch_number, "en" ) )
+
+from phonenumbers import carrier
+service_number=phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
+
+from phonenumbers import timezone
+time_zone=phonenumbers.parse(number)
+print(timezone.time_zones_for_number(time_zone))
+
